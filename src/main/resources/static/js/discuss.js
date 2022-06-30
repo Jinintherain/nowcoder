238b24dc32b53998a39659dd42044be91e7debf2
jin <2238377479@qq.com>
2022-06-30 11:17:43 +0800

    2022/6/30

--- src/main/resources/static/js/discuss.js
+++ src/main/resources/static/js/discuss.js
@@ -1,3 +1,9 @@
+$(function(){
+    $("#topBtn").click(setTop);
+    $("#wonderfulBtn").click(setWonderful);
+    $("#deleteBtn").click(setDelete);
+});
+
 function like(btn, entityType, entityId, entityUserId, postId) {
     $.post(
         CONTEXT_PATH + "/like",
@@ -13,3 +19,54 @@ function like(btn, entityType, entityId, entityUserId, postId) {
         }
     );
 }
+
+/ 置顶与取消置顶
+function setTop() {
+    $.post(
+        CONTEXT_PATH + "/discuss/top",
+        {"id":$("#postId").val()},
+        function(data) {
+            data = $.parseJSON(data);
+            if(data.code == 0) {
+                // $("#topBtn").attr("disabled", "disabled");
+                $("#topBtn").text(data.type==1?'取消置顶':'置顶');
+            } else {
+                alert(data.msg);
+            }
+        }
+    );
+}
+
+// 加精与取消加精
+function setWonderful() {
+    $.post(
+        CONTEXT_PATH + "/discuss/wonderful",
+        {"id":$("#postId").val()},
+        function(data) {
+            data = $.parseJSON(data);
+            if(data.code == 0) {
+                // $("#wonderfulBtn").attr("disabled", "disabled");
+                $("#wonderfulBtn").text(data.status==1?'取消加精':'加精');
+            } else {
+                alert(data.msg);
+            }
+        }
+    );
+}
+
+// 删除
+function setDelete() {
+    $.post(
+        CONTEXT_PATH + "/discuss/delete",
+        {"id":$("#postId").val()},
+        function(data) {
+            data = $.parseJSON(data);
+            if(data.code == 0) {
+                location.href = CONTEXT_PATH + "/index";
+            } else {
+                alert(data.msg);
+            }
+        }
+    );
+}
+
